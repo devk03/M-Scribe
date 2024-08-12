ebac7edb8cb2ea1e0f762de1176aa1f8cab630fc
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-08-12 00:12:31 -0400

    Working chat feature.

--- backend/routers/scraping/scrapingRoutes.py
+++ backend/routers/scraping/scrapingRoutes.py
@@ -32,14 +32,9 @@ async def fetch_lecture(request: Request):
     # Use the extracted PHPSESSID to make the request
     response = requests.get(url, cookies={"PHPSESSID": PHPSESSID})
 
-    # Parse the content of all timestamps from the response
-    print(">>> Removing timestamps")
     rawTranscript = removeTimestamps(response.content.decode("utf-8"))
-    print(">>> Parsing transcript")
     parsedTranscript = parseTranscript(rawTranscript)
-    print(">>> Adding delimiters")
     delimitedTranscript = add_delimiters(parsedTranscript)
-    print(">>> Posting vectors to Pinecone")
     process_and_post_text(delimitedTranscript, CAEN)
 
     return {"content": delimitedTranscript}
--- frontend/extension/chat/chatQuery.ts
+++ frontend/extension/chat/chatQuery.ts
@@ -3,7 +3,7 @@ export const chatQuery = async (lectureID: string, queryText: string) => {
     const settings = {
         method: 'POST',
         body: JSON.stringify({
-            lectureID: lectureID,
+            CAEN_ID: lectureID,
             queryText: queryText,
         }),
         headers: {
@@ -13,9 +13,10 @@ export const chatQuery = async (lectureID: string, queryText: string) => {
     };
 
     try {
-        console.log("Querying FastAPI Backend: ", process.env.PLASMO_PUBLIC_BACKEND);
+        console.log("Querying FastAPI Backend: ", `${process.env.PLASMO_PUBLIC_BACKEND}/query`);
         const fetchResponse = await fetch(`${process.env.PLASMO_PUBLIC_BACKEND}/query`, settings);
         const data = await fetchResponse.json();
+        console.log("Response from FastAPI Backend:", data);
         return data.response; // Assuming you want to return just the response string
     } catch (error) {
         console.error("Error querying FastAPI Backend:", error);
